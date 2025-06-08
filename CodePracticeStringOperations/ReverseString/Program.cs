144646e622c404ddfa1e9a75b3a80165df2a0361
Vaibhav Chavan <vaibhavchavan9990@gmail.com>
2025-06-08 19:41:00 +0530

    Commiting changes for reverse string code

---
+++ CodePracticeStringOperations/ReverseString/Program.cs
@@ -0,0 +1,30 @@
+﻿
+public class Reverse
+{
+    public string ReverseStr(string input)
+    {
+        char[] chars = input.ToCharArray();
+        string res = "";
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            res += chars[i];
+        }
+        return res;
+    }
+}
+
+class program
+{
+    static void Main(string[] strings)
+    {
+        string str = "CSharp";
+        Reverse reverse = new Reverse();
+        string Result = reverse.ReverseStr(str);
+
+        Console.WriteLine(Result);
+        Console.ReadKey();
+    }
+}
+
+
+
